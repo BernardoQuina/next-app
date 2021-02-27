0eba6e17f0f5baca3b62421a372fbabd270886b1
Bernardo-Quina <bernardoquina@gmail.com>
2021-02-27 11:14:55 +0000

    Changing development uri to /graphql: same thing needs to be done in production (vercer variables)

--- lib/apollo.ts
+++ lib/apollo.ts
@@ -16,7 +16,7 @@ const createClient = (_ctx: NextPageContext) => {
   let uri
 
   if (process.env.NODE_ENV === 'development') {
-    uri = 'http://localhost:4000/'
+    uri = 'http://localhost:4000/graphql'
   } else {
     uri = process.env.NEXT_PUBLIC_API_URL
   }
