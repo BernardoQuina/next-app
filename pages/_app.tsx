df33ca3e0dd442173601e24e1e8d19b9eefbc75e
Bernardo-Quina <bernardoquina@gmail.com>
2021-03-06 09:52:55 +0000

    Leave: display none

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,11 @@ import { useTransition, animated } from 'react-spring'
 
 const MyApp = ({ Component, pageProps, router }: AppProps) => {
   const transitions = useTransition(router, (router) => router.pathname, {
-    from: { opacity: 0, transform: 'scale(2)' },
+    from: { opacity: 0, transform: 'scale(1.5)' },
     enter: { opacity: 1, transform: ' scale(1)' },
-    leave: {
-      opacity: 0,
-      top: 0,
-      right: 0,
-      bottom: 0,
-      left: 0,
-      position: 'absolute',
-    },
+    leave: { display: 'none' },
+    initial: { opacity: 0 },
+    config: { mass: 1, tension: 500, friction: 100, clamp: true },
   })
 
   return (
