a0ced2160a8bf629659135e15b940d73e3cfd1ce
Bernardo-Quina <bernardoquina@gmail.com>
2021-03-07 07:51:13 +0000

    Route animations: different animations for home and other routes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,27 @@ const MyApp = ({ Component, pageProps, router }: AppProps) => {
   return (
     <motion.div
       key={router.route}
-      initial='pageInitial'
-      animate='pageAnimate'
+      initial={router.pathname === '/' ? 'homeInitial' : 'pageInitial'}
+      animate={router.pathname === '/' ? 'goHome' : 'pageAnimate'}
+      transition={{duration: 0.5}}
       variants={{
+        homeInitial: {
+          opacity: 0.5,
+          x: -800
+        },
         pageInitial: {
           opacity: 0,
+          scale: 0.5
+        },
+        goHome: {
+          opacity: 1,
+          x: 0
         },
         pageAnimate: {
           opacity: 1,
-        },
+          width: '100%',
+          scale: 1
+        }
       }}
     >
       <Component {...pageProps} />
